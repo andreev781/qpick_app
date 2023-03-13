b0a1ce2f956b50ff56c1ba02a243636f10d1f5bd
Daniil <62702483+NocokCydbi@users.noreply.github.com>
2023-03-13 23:39:06 +0300

    4

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,9 +1,5 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
-  server: {
-    "host": "0.0.0.0",
-    "port": "3000"
-  },
   loading: {
     color: 'blue',
     height: '5px'
